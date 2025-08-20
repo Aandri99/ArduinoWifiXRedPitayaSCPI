ed143f4c2c7ae2b1bca39eaef5bf27d42fb02ad2
AndrazPirc <114345840+Aandri99@users.noreply.github.com>
2025-08-21 00:38:49 +0200

    Update LEDsWifi.ino

--- examples/Wifi/LEDsWifi/LEDsWifi.ino
+++ examples/Wifi/LEDsWifi/LEDsWifi.ino
@@ -1,45 +1,22 @@
 #include "wifiSCPI.h"
-#include "arduino_secrets.h"
-
-// ------- User config -------
-IPAddress RP_IP(192, 168, 0, 17);
-const uint16_t RP_PORT = 5000;
-
-const float GEN_FREQ_HZ   = 2000.0;
-const float GEN_AMPL_V    = 1.0;
-const float GEN_OFFSET_V  = 0.0;
-// ---------------------------
+#include "arduino_secrets.h"   // WiFi + Red Pitaya settings
 
 WifiSCPI rp;
 
-void startGen() {
-  rp.scpi("GEN:RST");
-  rp.scpi("SOUR1:FUNC SINE");
-  rp.scpi(String("SOUR1:FREQ:FIX ") + String(GEN_FREQ_HZ, 6));
-  rp.scpi(String("SOUR1:VOLT ")     + String(GEN_AMPL_V, 6));
-  rp.scpi(String("SOUR1:VOLT:OFFS ")+ String(GEN_OFFSET_V, 6));
-  rp.scpi("OUTPUT1:STATE ON");
-  rp.scpi("SOUR1:TRig:SOUR INT");
-  rp.scpi("SOUR1:TRig:INT");
-}
-
 void setup() {
-  Serial.begin(115200);
-  delay(150);
+  // Connect to WiFi and Red Pitaya
+  rp.begin(SECRET_SSID, SECRET_PASS, SECRET_RP_IP, SECRET_RP_PORT);
 
-  if(!rp.begin(SECRET_SSID, SECRET_PASS, RP_IP, RP_PORT)){
-    Serial.println(F("Failed to connect"));
-    while(true){}
-  }
-
-  startGen();
+  // Set LED0 as output
+  rp.scpi("DIG:PIN:DIR LED0,OUT");
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) rp.connectWiFi(SECRET_SSID, SECRET_PASS);
-  if (!rp.connected()) {
-    if (rp.connectRP(RP_IP, RP_PORT)) startGen();
-  }
+  // Turn LED0 ON
+  rp.scpi("DIG:PIN LED0,1");
+  delay(500);
+
+  // Turn LED0 OFF
+  rp.scpi("DIG:PIN LED0,0");
   delay(500);
 }
-
